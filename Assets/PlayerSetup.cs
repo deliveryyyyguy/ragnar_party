b3bccc322e17a8502bf1209675276361da86e791
Rick King <king2rr@mail.uc.edu>
2018-04-24 21:48:32 -0400

    2

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -15,5 +15,20 @@ public class PlayerSetup : NetworkBehaviour {
                 componentsToDisable[i].enabled = false;
             }
         }
+
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
     }
 }
